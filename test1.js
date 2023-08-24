6e4091abfed9baf15f56fd3dc2e095ee5175a569
priyanka mohanty <111486056+priyankaui@users.noreply.github.com>
2023-08-24 19:55:14 +0530

    Create test1.js
    js assignment

---
+++ test1.js
@@ -0,0 +1,30 @@
+var students = [{
+    "name": "John",
+    "age": 22,
+    "hobby": {
+		"reading" : true,
+		"gaming" : false,
+		"sport" : "football"
+    },
+    "class" : ["JavaScript", "HTML", "CSS"]
+},
+{
+    "name": "sandeep",
+    "age": 35,
+    "hobby": {
+		"reading" : true,
+		"gaming" : false,
+		"sport" : "cricket"
+    },
+    "class" : ["React", "HTML", "CSS"]
+},
+{
+    "name": "Phani",
+    "age": 30,
+    "hobby": {
+		"reading" : true,
+		"gaming" : false,
+		"sport" : "cricket"
+    },
+    "class" : ["React"]
+}]
